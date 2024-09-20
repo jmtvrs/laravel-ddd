c758fe1fa5cd8155418fceec6c2f3dab37dd3158
Miguel Tavares <mgl.tvrs@gmail.com>
2024-09-20 19:29:13 +0100

    Add logging context middleware

---
+++ app/External/Web/Http/Middleware/LoggingContextMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\External\Web\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+
+class LoggingContextMiddleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        Log::shareContext(array_filter([
+            'hostname' => gethostname(),
+            'ip' => request()?->ip(),
+            'ua' => request()?->userAgent(),
+            'trace_id' => $request->header('X-Trace-ID') ?? Str::uuid()->toString(),
+        ]));
+
+        //        Context::add('hostname', gethostname());
+        //        Context::add('trace_id', $traceID);
+
+        return $next($request);
+    }
+}
