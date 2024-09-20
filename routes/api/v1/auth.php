8f4cb6c15cfef8c6a7f9df24b4a76b2b55cb2bba
Miguel Tavares <mgl.tvrs@gmail.com>
2024-09-20 19:46:10 +0100

    Get user endpoint

--- routes/api/v1/auth.php
+++ routes/api/v1/auth.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use App\External\Web\Http\Controllers\Api\V1\AuthenticationController;
 use App\External\Web\Http\Controllers\Api\V1\RegisterUserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::middleware('guest')->group(function () {
 
@@ -13,5 +14,14 @@ Route::middleware('guest')->group(function () {
     Route::post('login', [AuthenticationController::class, 'login'])->name('login');
 });
 
-Route::post('logout', [AuthenticationController::class, 'logout'])
-    ->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->name('logged-in-user');
+
+    Route::post('logout', [AuthenticationController::class, 'logout'])->name('logout');
+});
+
+
+
